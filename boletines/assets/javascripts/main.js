8eb90ee9b36636358b7652a5de55ef4c743a9013
Camilo Suarez <juan.suarez.quinones@unillanos.edu.co>
2020-04-01 19:28:41 -0500

    eventos de salidas para las paginas

--- boletines/assets/javascripts/main.js
+++ boletines/assets/javascripts/main.js
@@ -1,1 +1,38 @@
-console.log("Hola mundo");/* print */
+/* 
+console.log("Hola mundo");/* print
+DOM
+let links = document.querySelectorAll("a");
+links.forEach(function(link){
+    console.log(link);
+});recorrer arreglos
+  */
+
+/* let celdas= document.querySelectorAll("td");
+
+celdas.forEach(function(td){
+    td.addEventListener("click",function(){
+        console.log(this);
+    });
+}); */
+
+let links = document.querySelectorAll(".close");
+
+links.forEach(function(link){
+    link.addEventListener("click",function(ev){
+        ev.preventDefault();
+        let content = document.querySelector(".content");
+
+        content.classList.remove("bounceInUp");
+        content.classList.remove("animated");
+
+        content.classList.add("bounceOutUp");
+        content.classList.add("animated");
+        
+        setTimeout(function(){ /* retardar la aparicion */
+             location.href = "/";
+        },600);
+       
+
+        return false;
+    });
+});
