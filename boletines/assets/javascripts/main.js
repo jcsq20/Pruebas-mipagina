8f7bb902f4b959bdccfce0e6c22fa1ee76065b27
Camilo Suarez <juan.suarez.quinones@unillanos.edu.co>
2020-04-01 19:44:16 -0500

    ruta

--- boletines/assets/javascripts/main.js
+++ boletines/assets/javascripts/main.js
@@ -31,7 +31,7 @@ links.forEach(function(link){
         
         setTimeout(function(){ /* retardar la aparicion */
              /* location.href = "/"; */
-             location.href = "/Mipagina/boletines/";
+             location.href = "../";
         },600);
        
 
